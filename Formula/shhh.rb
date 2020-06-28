a30198fb55afa08f5a9257ddb24e373214de7c80
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2020-06-28 22:53:31 +0100

    Bump shhh versionb

--- Formula/shhh.rb
+++ Formula/shhh.rb
@@ -5,7 +5,7 @@ class Shhh < Formula
   desc "GO cli client for Shhh"
   homepage "https://github.com/smallwat3r/shhh-cli"
   url "https://github.com/smallwat3r/shhh-cli/releases/download/1.1.6/shhh-1.1.6-homebrew.tar.gz"
-  version "1.1.5"
+  version "1.1.6"
   sha256 "ac07dbe2e0f349efb46ef9559185afcf4358cc78970a53f882e5e0844d6acfb2"
 
   def install
