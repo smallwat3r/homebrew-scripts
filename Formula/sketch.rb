bdf08c29d1fa7c26afb87d489fdd6470038baefc
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2020-07-21 21:06:18 +0100

    Bump sketch

--- Formula/sketch.rb
+++ Formula/sketch.rb
@@ -4,9 +4,9 @@
 class Sketch < Formula
   desc "Create and edit draft files on the go from your terminal in your favourite text editor."
   homepage "https://github.com/smallwat3r/sketch"
-  url "https://github.com/smallwat3r/sketch/archive/0.3.tar.gz"
-  version "0.3"
-  sha256 "53b2d3db96ac595ed03f15a18e4c5afaca43634db5a5ad924e0f49953943b5cf"
+  url "https://github.com/smallwat3r/sketch/archive/0.3.1.tar.gz"
+  version "0.3.1"
+  sha256 "ba81290271262fa1fd8a2b1aa4003093c653c459933a577672ffc28aaf06c3f8"
 
   def install
     bin.install "sketch"
