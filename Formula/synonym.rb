4d74dda1b82866e4faaad06e4ec2ff27abcbfaa5
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2020-07-22 14:15:39 +0100

    Bump synonym

--- Formula/synonym.rb
+++ Formula/synonym.rb
@@ -4,9 +4,9 @@
 class Synonym < Formula
   desc "Find synonyms in 15 different languages directly from your terminal."
   homepage "https://github.com/smallwat3r/synonym"
-  url "https://github.com/smallwat3r/synonym/archive/1.4.1.tar.gz"
-  version "1.4.1"
-  sha256 "3e0c698705ebf63e99e327c317c23a374171480fc6dad2c715d444fedd2b135b"
+  url "https://github.com/smallwat3r/synonym/archive/1.4.2.tar.gz"
+  version "1.4.2"
+  sha256 "dcf782e38d5a1891386c286f7d1216c118b7f8d0b8b72541b5013e3d7c68b1ea"
 
   depends_on "jq"
 
