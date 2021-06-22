78d976a79eb81fa0325d9bb906fe9c7411e713f7
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2021-06-22 23:45:35 +0100

    Publish updated version of shhh-cli

--- Formula/shhh.rb
+++ Formula/shhh.rb
@@ -1,9 +1,9 @@
 class Shhh < Formula
   desc "GO cli client for Shhh"
   homepage "https://github.com/smallwat3r/shhh-cli"
-  url "https://github.com/smallwat3r/shhh-cli/releases/download/1.2.1/shhh-1.2.1-homebrew.tar.gz"
-  version "1.2.1"
-  sha256 "cff0e05ea00a30a1c54d0bfec75186a1769abb6331ae5be3ecc9205319e3db2a"
+  url "https://github.com/smallwat3r/shhh-cli/releases/download/1.3.0/shhh-1.3.0-homebrew.tar.gz"
+  version "1.3.0"
+  sha256 "074199663ec609f1e6e0503c99153eb4b77dd3bd1379cace6daba59cec23d832"
 
   def install
     bin.install "shhh"
