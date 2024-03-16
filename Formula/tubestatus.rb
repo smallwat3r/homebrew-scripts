29e85d53830a9c665da61dd0cf16bffbd4920d73
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2024-03-16 15:31:45 +0000

    Bump tubestatus version

--- Formula/tubestatus.rb
+++ Formula/tubestatus.rb
@@ -1,9 +1,9 @@
 class Tubestatus < Formula
   desc "London Tube live status from your terminal using the TFL API."
   homepage "https://github.com/smallwat3r/tubestatus"
-  url "https://github.com/smallwat3r/tubestatus/archive/1.2.4.tar.gz"
-  version "1.2.4"
-  sha256 "bb914c0a7099acf8b1657688fbc9a591062a0fb8dd9a1bb286cb654f26a1e102"
+  url "https://github.com/smallwat3r/tubestatus/archive/1.2.5.tar.gz"
+  version "1.2.5"
+  sha256 "41410daf4bb524339e5f72dae59ce90700dbd5a732b5af12d48da26d24e4226e"
 
   depends_on "jq"
 
