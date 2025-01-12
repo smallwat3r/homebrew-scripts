4b93cdad15a34909845bdfed9ef59daf641dfc72
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2025-01-12 17:03:36 +0000

    Add new synonym version

--- Formula/synonym.rb
+++ Formula/synonym.rb
@@ -1,9 +1,9 @@
 class Synonym < Formula
   desc "Find synonyms in 15 different languages directly from your terminal."
   homepage "https://github.com/smallwat3r/synonym"
-  url "https://github.com/smallwat3r/synonym/archive/1.4.3.tar.gz"
-  version "1.4.3"
-  sha256 "14d49cb30616195a213d473422f6b72ab4f8cf04015126833756b587b5845e98"
+  url "https://github.com/smallwat3r/synonym/archive/1.4.4.tar.gz"
+  version "1.4.4"
+  sha256 "43486abeb4e8293d4ecb3bda6fdcd5486d99d0c6f65a3921d4d3053f50b293f7"
 
   depends_on "jq"
 
