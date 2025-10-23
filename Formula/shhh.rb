562696fbbca8c84b7de130fbb96ac90aed02ca8f
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2025-10-23 23:12:36 +0100

    Add new formula for shhh

--- Formula/shhh.rb
+++ Formula/shhh.rb
@@ -1,11 +1,25 @@
 class Shhh < Formula
   desc "GO cli client for Shhh"
   homepage "https://github.com/smallwat3r/shhh-cli"
-  url "https://github.com/smallwat3r/shhh-cli/releases/download/1.3.1/shhh-1.3.1-homebrew.tar.gz"
-  version "1.3.1"
-  sha256 "f2e9a8ee67558ae923d3355eb08986cda9bc047ad5d67867857ad98e2b5e0f50"
+  version "1.3.3"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/smallwat3r/shhh-cli/releases/download/1.3.3/shhh_darwin_arm64.tar.gz"
+      sha256 "5b310c62f145f6838759b04307ae9132f54b57ec7ac4dcc57772e07c99d7b208"
+    end
+
+    on_intel do
+      url "https://github.com/smallwat3r/shhh-cli/releases/download/1.3.3/shhh_darwin_amd64.tar.gz"
+      sha256 "b0c2e7fa8bb5e4e78655ae8886b3edc66c30493ace28bcde96fd9d9430908e88"
+    end
+  end
 
   def install
-    bin.install "shhh"
+    bin.install "shhh-darwin-#{Hardware::CPU.arch}" => "shhh"
+  end
+
+  test do
+    system "#{bin}/shhh", "-v"
   end
 end
